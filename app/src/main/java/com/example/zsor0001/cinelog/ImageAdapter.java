1dea4014ce168f91700f9317fede5d569d7c09e8
Zita Soroka <z.lucky7@gmail.com>
2016-07-19 20:47:14 +0100

    Update ImageAdapter.java

--- app/src/main/java/com/example/zsor0001/cinelog/ImageAdapter.java
+++ app/src/main/java/com/example/zsor0001/cinelog/ImageAdapter.java
@@ -25,9 +25,8 @@ public class ImageAdapter extends ArrayAdapter<String> {
      */
     public ImageAdapter(Activity context, List<String> thumbs) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
+        // the second argument is used when the ArrayAdapter is populating a single ImageView.
+        
         super(context, 0, thumbs);
     }
 
